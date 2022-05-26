f64aec3a95ce285fdf7b27d9e2ec3e3054b99f64
DMosqueral <dml2410@gmail.com>
2022-05-26 00:55:05 -0500

    "ADD --- Se resolvieron las Kata6, kata7 y Kata8"

--- src/main/java/katas/Kata8.java
+++ src/main/java/katas/Kata8.java
@@ -1,15 +1,13 @@
 package katas;
 
 import com.codepoetics.protonpack.StreamUtils;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
 import model.Movie;
-import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Combine videos and bookmarks by index (StreamUtils.zip) (https://github.com/poetix/protonpack)
@@ -21,8 +19,15 @@ public class Kata8 {
         List<Movie> movies = DataUtil.getMovies();
         List<Bookmark> bookMarks = DataUtil.getBookMarks();
 
-        // StreamUtils.zip()
+        var peliculas = movies.stream()
+                .map(e -> e.getId());
 
-        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
+        var books = bookMarks.stream()
+                .map(e -> e.getId());
+
+        return StreamUtils.zip(peliculas, books, (peli, book) -> Map.of("videoId", peli, "bookmarkId", book))
+                .collect(Collectors.toList());
+
+//        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
     }
 }
